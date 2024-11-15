b36bee871187b4ea2f4ccadc43664a74b795b391
zwiadowca987 <mat.szczerek@gmail.com>
2024-11-15 12:36:08 +0100

    Cleaning Project

--- ProjektZespolowy/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjektZespolowy.Models
-{
-	public class ErrorViewModel
-	{
-		public string? RequestId { get; set; }
-
-		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-	}
-}
